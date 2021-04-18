596947bc33723d4e91015cbbd3a9a8713cb01116
Hilmi-Mehdi <76878548+Hilmi-Mehdi@users.noreply.github.com>
2021-04-18 15:04:56 +0000

    Add files via upload

---
+++ brk-pff-40bc36dfdf28d284c2fb542311e999a88fde4701/Sceenes/profil.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+const profil=()=> {
+  return (
+    <View>
+        <Image/>
+        <Text></Text>
+        
+    </View>
+  );
+};
+
+export default profil
---
+++ brk-pff-40bc36dfdf28d284c2fb542311e999a88fde4701/icons/icons.js
@@ -0,0 +1,11 @@
+export const profil =require('gg_profile.png');
+export const orders =require('orders.png');
+export const offre =require('offre.png');
+export const privacy =require('privacy.png');
+export const logout =require('signout.png');
+
+export  default {profil,
+    orders,
+    offre,
+    privacy,
+    slogout}
